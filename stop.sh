5f824cc307956edc36025e499fa1fd0e8554ab42
alebocci <alessandro.bocci@phd.unipi.it>
2024-04-18 10:45:10 +0200

    updated compose

--- stop.sh
+++ stop.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if uname -m | grep -iq "arm"; then
-    docker compose -f compose.yml -f arm.yml up -d
+    docker compose -f compose.yml -f arm.yml down
 else
     docker compose -f compose.yml down
 fi
